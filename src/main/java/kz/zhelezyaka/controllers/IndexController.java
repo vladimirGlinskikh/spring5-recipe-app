b855d706a49033383922e0b1b02ee2772c4fcf8d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-22 23:02:24 +0600

    Spring Boot development tools

--- src/main/java/kz/zhelezyaka/controllers/IndexController.java
+++ src/main/java/kz/zhelezyaka/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        System.out.println("some test reload...");
         return "index";
     }
 }
