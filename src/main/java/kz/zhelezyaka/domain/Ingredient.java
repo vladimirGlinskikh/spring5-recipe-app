69d8b9337c107cf66539e33370d73921ed1bf46e
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-02-21 22:21:16 +0600

    Pro-Tips - Using Setters for JPA bidirectional relationships

--- src/main/java/kz/zhelezyaka/domain/Ingredient.java
+++ src/main/java/kz/zhelezyaka/domain/Ingredient.java
@@ -18,7 +18,10 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-    public Ingredient() {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
--- src/main/java/kz/zhelezyaka/domain/Recipe.java
+++ src/main/java/kz/zhelezyaka/domain/Recipe.java
@@ -35,7 +35,7 @@ public class Recipe {
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
-    joinColumns = @JoinColumn(name = "recipe_id"),
+            joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
@@ -117,6 +117,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
