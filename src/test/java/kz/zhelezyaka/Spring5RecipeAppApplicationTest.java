78677055af96377c8256f7dd1c4807a4dd1ed672
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-02 23:54:24 +0600

    Creating a JUnit Test

---
+++ src/test/java/kz/zhelezyaka/Spring5RecipeAppApplicationTest.java
@@ -0,0 +1,15 @@
+package kz.zhelezyaka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RecipeAppApplicationTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
